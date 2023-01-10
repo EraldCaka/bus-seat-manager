0f54a4c0dc374dd0ff94a5ab3a62553c1fd2fe2c
Erald Caka <96385473+HidekiNatsumi@users.noreply.github.com>
2023-01-10 23:23:56 +0100

    Add files via upload

---
+++ bus.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="bus.css" />
+    <title>Bus</title>
+  </head>
+  <body>
+    <div class="bus">
+
+      <h1>Bus seat manager</h1>
+      <a class="g">green : free</a>
+      <a class="y">yellow : reserved</a>
+      <a class="r">red : occupied</a>
+
+      <div class="bus__row">
+        <div class="bus__seat">
+          <button  class="seat_now1 seat_now" id="1A" value="1A" >1A</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now2 seat_now" id="1B" value="1B" >1B</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now3 seat_now" id="1C" value="1C" >1C</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now4 seat_now" id="1D" value="1D" >1D</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now5 seat_now" id="1E" value="1E" >1E</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now6 seat_now" id="1F" value="1F" >1F</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now7 seat_now" id="1G" value="1G" >1G</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now8 seat_now" id="1H" value="1H" >1H</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now9 seat_now" id="1I" value="1I" >1I</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now10 seat_now" id="1J" value="1J" >1J</button>
+        </div>
+      </div>
+      <div class="bus__row">
+        <div class="bus__seat">
+          <button  class="seat_now11 seat_now" id="2A" value="2A" >2A</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now12 seat_now" id="2B" value="2B" >2B</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now13 seat_now" id="2C" value="2C" >2C</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now14 seat_now" id="2D" value="2D" >2D</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now15 seat_now" id="2E" value="2E" >2E</button>
+        </div>
+
+        <div class="bus__seat">
+          <button  class="seat_now16 seat_now" id="2F" value="2F" >2F</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now17 seat_now" id="2G" value="2G" >2G</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now18 seat_now" id="2H" value="2H" >2H</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now19 seat_now" id="2I" value="2I" >2I</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now20 seat_now" id="2J" value="2J" >2J</button>
+        </div>
+      </div>
+
+      <div class="bus__row">
+        <div class="bus__seat">
+          <button   class="seat_now21 seat_now" id="3A" value="3A" >3A</button>
+        </div>
+
+        <div class="bus__seat">
+          <button  class="seat_now22 seat_now" id="3B" value="3B" >3B</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now23 seat_now" id="3C" value="3C" >3C</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now24 seat_now" id="3D" value="3D" >3D</button>
+        </div>
+
+        <div class="bus__seat">
+          <button  class="seat_now25 seat_now" id="3E" value="3E" >3E</button>
+        </div>
+
+        <div class="bus__seat">
+          <button  class="seat_now26 seat_now" id="3F" value="3F" >3F</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now27 seat_now" id="3G" value="3G" >3G</button>
+        </div>
+
+        <div class="bus__seat">
+          <button  class="seat_now28 seat_now" id="3H" value="3H" >3H</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now29 seat_now" id="3I" value="3I" >3I</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now30 seat_now" id="3J" value="3J" >3J</button>
+        </div>
+      </div>
+
+      <div class="bus__row">
+        <div class="bus__seat">
+          <button  class="seat_now31 seat_now" id="4A" value="4A" >4A</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now32 seat_now" id="4B" value="4B" >4B</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now33 seat_now" id="4C" value="4C" >4C</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now34 seat_now" id="4D" value="4D" >4D</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now35 seat_now" id="4E" value="4E" >4E</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now36 seat_now" id="4F" value="4F" >4F</button>
+        </div>
+
+        <div class="bus__seat">
+          <button  class="seat_now37 seat_now" id="4G" value="4G" >4G</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now38 seat_now" id="4H" value="4H" >4H</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now39 seat_now"  id="4I" value="4I" >4I</button>
+        </div>
+        <div class="bus__seat">
+          <button  class="seat_now40 seat_now" id="4J" value="4J" >4J</button>
+        </div>
+      </div>
+      <form   method="post"  >
+        <div class="form1">
+      <input type="text" name="seatname" class="seat_name" placeholder="seat name" required/>
+      <input type="text" name="seatstatus" class="seat_status" placeholder="status" required/>
+        </div>
+        <div class="form1">
+      <button class="change" name="update" type="submit"  >Submit</button>
+        </div>
+      </form>
+
+
+  
+    </div>
+    <?php
+    // error_reporting(0);
+include 'dbconn.php';
+$sql = 'SELECT seatscol,status_t FROM bus.seats;';
+           
+  $result = mysqli_query($conn, $sql);
+
+  $seats = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+  mysqli_free_result($result);
+
+  $seats_count=count($seats);
+
+  echo "<br>";
+  for($i=0;$i<count($seats);$i++){
+   // echo $seats[$i]['seatscol'] , " ";
+  }
+  echo "<br>";
+  for($i=0;$i<count($seats);$i++){
+  //  echo $seats[$i]['status_t'] , "   ";
+  }
+  echo "<br>";
+/*
+  echo"<script>localStorage.setItem('seats_count','$seats_count');";
+            for($i =0; $i<$seats_count;$i++){
+              if($seats[$i]['status_t'] == 'o'){
+                $temp=$i+1;
+           
+                echo "localStorage.setItem('color$temp','');";
+              
+              }
+              if($seats[$i]['status_t'] == 'f'){
+                $temp=$i+1;
+           
+                echo "localStorage.setItem('color$temp','');";
+              
+              }
+              if($seats[$i]['status_t'] == 'r'){
+                $temp=$i+1;
+           
+                echo "localStorage.setItem('color$temp','');";
+              
+              }
+            }
+            echo"</script>";
+*/
+  echo"<script>localStorage.setItem('seats_count','$seats_count');";
+            for($i =0; $i<$seats_count;$i++){
+              if($seats[$i]['status_t'] == 'o'){
+                $temp=$i+1;
+           
+                echo "localStorage.setItem('color$temp','#C24E4E');";
+              
+              }
+              if($seats[$i]['status_t'] == 'f'){
+                $temp=$i+1;
+           
+                echo "localStorage.setItem('color$temp','#4caf50');";
+              
+              }
+              if($seats[$i]['status_t'] == 'r'){
+                $temp=$i+1;
+           
+                echo "localStorage.setItem('color$temp','#dbdb65');";
+              
+              }
+            }
+            echo"</script>";
+
+    ?>
+    <?php
+  if(isset($_POST['update'])){
+      $seat_name = $_POST['seatname'];
+
+      $seat_status = $_POST['seatstatus'];
+      for($i=0;$i<count($seats);$i++){
+        if($seat_name == $seats[$i]['seatscol']){
+          if(str_starts_with($seat_status,"f")){
+       //  echo "nis me f!";
+         $update = "UPDATE bus.seats SET status_t = 'f' WHERE seatscol = '$seat_name';";
+         $query_run = mysqli_query($conn,$update);
+         if (mysqli_query($conn, $update)) {
+          if($query_run)
+          {
+          }
+        
+        } else {
+          echo '<script type="text/javascript"> alert("Data Not Updated") </script>';
+        }
+
+          }
+          if(str_starts_with($seat_status,"r")){
+            $update = "UPDATE seats SET status_t = 'r' WHERE seatscol = '$seat_name';";
+            if (mysqli_query($conn, $update)) {
+           
+           
+            } else {
+              echo '<script type="text/javascript"> alert("Data Not Updated") </script>';
+            }
+            
+      
+     
+          }
+          if(str_starts_with($seat_status,"o")){
+            $update = "UPDATE bus.seats SET status_t = 'o' WHERE seatscol = '$seat_name';";
+            $query_run = mysqli_query($conn,$update);
+            if (mysqli_query($conn, $update)) {
+              header("Location: bus.php");
+           
+            } else {
+              echo '<script type="text/javascript"> alert("Data Not Updated") </script>';
+            }
+
+          }
+        }
+      
+     
+      }
+  }
+    ?>
+    <script>
+    for (let i = 0; i < localStorage.getItem("seats_count"); i++) {
+  if (localStorage.getItem(`color${i + 1}`) == "#C24E4E") {
+    document.querySelector(`.seat_now${i + 1}`).style.backgroundColor = "#C24E4E";
+  }
+  if (localStorage.getItem(`color${i + 1}`) == "#dbdb65") {
+    document.querySelector(`.seat_now${i + 1}`).style.backgroundColor = "#dbdb65";
+  }
+  if (localStorage.getItem(`color${i + 1}`) == "#4caf50") {
+    document.querySelector(`.seat_now${i + 1}`).style.backgroundColor = "#4caf50";
+  }
+}
+document.querySelector(".change").addEventListener("click", function (e) {
+for (let i = 0; i < localStorage.getItem("seats_count"); i++) {
+  if (localStorage.getItem(`color${i + 1}`) == "#C24E4E") {
+    document.querySelector(`.seat_now${i + 1}`).style.backgroundColor = "#C24E4E";
+  }
+  if (localStorage.getItem(`color${i + 1}`) == "#dbdb65") {
+    document.querySelector(`.seat_now${i + 1}`).style.backgroundColor = "#dbdb65";
+  }
+  if (localStorage.getItem(`color${i + 1}`) == "#4caf50") {
+    document.querySelector(`.seat_now${i + 1}`).style.backgroundColor = "#4caf50";
+  }
+}
+});
+</script>
+    <script src="bus.js"></script>
+  </body>
+</html>
